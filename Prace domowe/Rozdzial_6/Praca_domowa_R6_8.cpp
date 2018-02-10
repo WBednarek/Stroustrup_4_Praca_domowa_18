0aa350ba95038388b1d3158053db7fcad7539a69
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-02-10 04:30:53 +0100

    Stroustrup PPP Exercise 8 Chapter 6 main functionaluty game is done.

--- Prace domowe/Rozdzial_6/Praca_domowa_R6_8.cpp
+++ Prace domowe/Rozdzial_6/Praca_domowa_R6_8.cpp
@@ -6,7 +6,7 @@ ostream & operator<< (ostream & output, vector<T> & to_disp)
 {
 	for (T disp : to_disp)
 	{
-		output << disp << " ";
+		output << disp;
 	}
 	return output;
 }
@@ -17,21 +17,115 @@ vector<T> display_vector1(vector<T> & to_display)
 	return to_display;
 }
 
+
+
 void praca_domowa_R6_8()
-{
-
-	char test = ' ';
-	vector<char> letters;
-
-	while (cin >> test)
+{	
+	vector<char> my_word = { 'd', 'a', 'r', 'o' };
+	cout << "Gues the word only english letters are accepted\nmaximun number of letters is " << my_word.size() << endl;
+	cout << "All characters after 4th will be ignored" << endl;
+	
+	while (true)
 	{
+		char test = ' ';
+		int letters_counter = 0;
+		vector<char> letters;
+		bool wrong_letters_num = false;
 		if (test == '.') break;
-		cin.putback(test);
-		get_letter(letters);
+		while (letters_counter != my_word.size())
+		{
+			++letters_counter;
+			if (letters_counter > my_word.size()) 
+			{	
+				wrong_letters_num = true; 
+				break; 
+			}
+			cin >> test;
+			if (test == '.') break;
+			cin.putback(test);
+			get_letter(letters);		
+		}
+		if (wrong_letters_num)
+		{
+			cout << "Wrong length of input, try again" << endl;
+		}
+		else
+		{
+			Bulls_and_cows b_and_c = cacluate_hits(my_word, letters);
+			if (b_and_c.bulls == my_word.size())
+			{
+				cout << "Bravo you guessed" << endl;
+				break;
+			}
+			else
+			{
+				cout << "Considered input word: \"" << letters << "\"" << endl;
+				cout << "Bull " << b_and_c.bulls << " Cow " << b_and_c.cows << endl;
+				cin.ignore(INT_MAX, '\n');
+			}
+		}
 		
+
 	}
 
-	cout << "Vector contains " << display_vector1(letters) << endl;
+
+}
+
+bool contains(char letter, vector<char> & letters)
+{
+	bool is_contain = false;
+	for (int i = 0; i < letters.size(); ++i)
+	{
+		if (letter == letters[i])
+		{
+			is_contain = true;
+			break;
+		}
+	}
+	return is_contain;
+}
+
+vector<char> make_unique_val_vector(vector<char> & letters)
+{
+	vector<char> unique_val;
+	unique_val.push_back(letters[0]);
+	for (int i = 1; i < letters.size(); ++i)
+	{
+		if (!contains(letters[i], unique_val))
+		{
+			unique_val.push_back(letters[i]);
+		}
+	}
+
+	return unique_val;
+}
+
+Bulls_and_cows cacluate_hits(vector<char> & to_check, vector<char> & checking)
+{
+	if (to_check.size() != checking.size()) error("Size of input stream mismatch with size of the word to check");
+	Bulls_and_cows bulls_and_cows;
+	bulls_and_cows.bulls = 0;
+	bulls_and_cows.cows = 0;
+
+	//Calculate bulls
+	for (int i = 0; i < to_check.size(); ++i)
+	{
+		if (to_check.at(i) == checking.at(i)) ++bulls_and_cows.bulls;
+	}
+	vector<char> unique_values = make_unique_val_vector(checking);
+
+	//Calculate cows
+	for (int i = 0; i < unique_values.size(); ++i)
+	{
+		for (int j = 0; j < to_check.size(); ++j)
+		{
+			if (unique_values.at(i) == to_check.at(j)) ++bulls_and_cows.cows;
+		}
+	}
+
+	bulls_and_cows.cows -= bulls_and_cows.bulls;
+
+	return bulls_and_cows;
 
 }
 
--- Prace domowe/Rozdzial_6/Praca_domowa_R6_8.h
+++ Prace domowe/Rozdzial_6/Praca_domowa_R6_8.h
@@ -2,10 +2,25 @@
 #include "../../../../../std_lib_facilities_v2.h"
 //#include "../Tools.h"
 
+
+struct Bulls_and_cows
+{
+	int bulls;
+	int cows;
+
+};
+
 void praca_domowa_R6_8();
+
+bool contains(char letter, vector<char>& letters);
+
+vector<char> make_unique_val_vector(vector<char>& letters);
+
+Bulls_and_cows cacluate_hits(vector<char>& to_check, vector<char>& checking);
 
 
 void get_letter(vector<char> & letters);
 
 template<typename T>
 vector<T> display_vector1(vector<T> & to_display);
+
