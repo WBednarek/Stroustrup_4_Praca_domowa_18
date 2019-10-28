5efb43ecee1320c9b1fd88584b6e1fb511f1366a
Wiktor Bednarek <wdbednarek@quinnipiac.edu>
2019-10-28 19:39:24 +0100

    Homework 11 Chapter 17 is done. I added move() function additionally.

--- Prace domowe/Rozdział_17/Homework_17.cpp
+++ Prace domowe/Rozdział_17/Homework_17.cpp
@@ -147,12 +147,315 @@ namespace Hw17
 
 	}
 
+	void hw6()
+	{
+		long long num_to_allocate = 122337203;
+		long long ll_size = sizeof(long long);
+		
+		cout << "long long size is: " << ll_size<< endl; 
+		long long allocated_num_size_kb = ll_size * num_to_allocate / 1000;
+		cout << "allocated num size is: " << allocated_num_size_kb << " kb" << endl;
+
+		long long allocated_num_size_gb = ll_size * num_to_allocate / 1000000000;
+		cout << "allocated num size is: " << allocated_num_size_gb << " gb" << endl;
+		
+		/*long long w = LLONG_MAX;
+		cout << "long long max is: " << w << endl;*/
+		int i = 1;
+		long long* a = new long long(num_to_allocate);
+		/*while (1)
+		{
+			
+			++i;
+			if(i%100000 = )
+		}*/
+
+		//CTRL+ALT+ESC in windows to open task manager of you can see memory usage in the Visual studio
+	}
+
+	Link* Link::erase()
+	{
+		if (this == 0) return 0;
+		if(succ) succ->prev = prev;// or this->succ->prev = prev;
+		if(prev) prev->succ = succ;
+		return succ;
+	}
+
+	Link* Link::find(const string& s)
+	{
+		Link* p = this;
+		while (this)
+		{
+			if (p == 0) return 0;
+			if (p->succ)
+			{
+				if (p->value == s) return p;
+				p = p->succ;
+			}
+			else
+			{
+				break;
+			}
+		}
+		while (this)
+		{
+			if (p == 0) return 0;
+			if (p->prev)
+			{
+				if (p->value == s) return p;
+				p = p->prev;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return 0;
+	}
+
+	Link* Link::find_rev(const string& s)
+	{
+		Link* p = this;
+		while (this)
+		{
+			if (p == 0) return 0;
+			if (p->value == s) return p;
+			p = p->prev;
+		}
+		return 0;
+	}
+
+	
+	Link* Link::move(int n) // not fully works
+	{
+		if (n < 0)
+		{
+			while (n++)// going left
+			{
+				//Borr, Freia, Odyn, Thor - moving Odyn
+				if (prev == 0) return this;
+				if (prev->prev)
+				{
+					prev->prev->succ = this; // Borr-> succ = Odyn
+				}
+				prev->succ = succ; //Freia-> succ = Thor
+				Link* tmp = prev->prev; 
+				prev->prev = this;
+				if (succ)
+				{
+					succ->prev = prev; //!	Thor->prev = Freia
+				}
+				succ = prev;
+				prev = tmp;
+			}
+		}
+
+		if (n > 0)
+		{
+			while (n--)// going right
+			{
+				//Borr, Freia, Odyn, Thor - moving Freia
+				if (succ == 0) return this;
+				if (succ->succ)
+				{
+					succ->succ->prev = this; //Thor->prev = Freia
+				}
+				succ->prev = prev; //Odyn->prev = Borr
+				Link* tmp = succ->succ;
+				succ->succ = this;
+				if (prev)
+				{
+					prev->succ = succ; //Borr-> succ = Odyn	
+				}
+				prev = succ;
+				succ = tmp;
+			}
+		}
+
+
+		return this;
+	}
+
+/*
+		Link* p = this;
+		if (p == 0) return 0;
+		if (n > 0)
+		{
+			while (n--) // going right
+			{
+				if (p->succ == 0) return 0;
+				if(p->prev == 0) 
+				p->prev->succ = p->succ;
+				p->succ->prev = p->prev;
+				Link* tmp = p->succ->succ;
+				p->succ->succ = p;
+				p->prev = p->succ;
+				p->succ = tmp;
+				//delete tmp;
+
+
+
+				Link* Link::insert(Link * n)
+				{
+					if (n == 0) return this;
+					if (this == 0) return n;
+					n->succ = this;
+					if (prev) prev->succ = n;
+					n->prev = prev;
+					prev = n;
+					return n;
+				}
+
+			}
+		}
+
+		*/
+
+
+
+
+	Link* Link::advance(int n) 
+	{
+		Link* p = this;
+		if (p == 0) return 0;
+		if (n < 0)
+		{
+			while (n++)
+			{
+				if (p->prev == 0) return 0;
+				p = p->prev;
+			}
+		}
+
+		if (n > 0)
+		{
+			while (n--)
+			{
+				if (succ == 0) return 0;
+				p = p->succ;
+			}
+		}
+
+		return p;
+	}	
+	
+	
+
+	Link* advance1(Link* p, int n)
+	{
+		
+		if (p == 0) return 0;
+		if (0<n)
+		{
+			while (n--) // going right
+			{
+				if (p->next() == 0) return 0;
+				p = p->next();
+			}
+		}
+		if (n < 0)
+		{
+			while (n++)// going left
+			{
+				if (p->previous() == 0) return 0;
+				p = p->previous();
+			}
+		}
+
+		return p;
+	}
+
+
+	Link* Link::insert(Link* n)
+	{
+		if (n == 0) return this;
+		if (this == 0) return n;
+		n->succ = this;
+		if (prev) prev->succ = n;
+		n->prev = prev;
+		prev = n;
+		return n;
+	}
+
+	void print_all(Link* p)
+	{
+		cout << "{";
+		while (p)
+		{
+			cout << p->value;
+			if (p = p->next()) cout << ", ";
+		}
+		cout << "}";
+	}
+
+
+	Link* Link::add(Link* n)
+	{
+		if (n == 0) return this;
+		if (this == 0) return n;
+		n->prev = this;
+		if (succ)
+		{
+			succ->prev = n;
+		}	
+		n->succ = succ;
+		succ = n;
+
+		return n;
+	}
+
+
+	void hw11()
+	{
+		Link* norse_gods = new Link("Thor");
+		norse_gods = norse_gods->insert(new Link("Odyn"));
+		norse_gods = norse_gods->insert(new Link("Zeus"));
+		norse_gods = norse_gods->insert(new Link("Freia"));
+
+		Link* greek_gods = new Link("Hera");
+		greek_gods = greek_gods->add(new Link("Atena"));
+		greek_gods = greek_gods->add(new Link("Mars"));
+		greek_gods = greek_gods->add(new Link("Posejdon"));
+		greek_gods = greek_gods->add(new Link("Atreus"));
+		greek_gods = greek_gods->insert(new Link("Kratos")); // before Atreus
+		
+
+		Link* p = greek_gods->find("Mars");
+		if (p) p->value = "Ares";
+
+		Link* p2 = norse_gods->find("Zeus");
+		if (p2)
+		{
+			if (p2 == norse_gods) norse_gods = p2->next();
+			p2->erase();
+			greek_gods = greek_gods->insert(p2);
+		}
+		
+
+		Link* p3 = greek_gods->find("Atena");
+		p3->move(22);
+		
+		greek_gods = greek_gods->find_rev("Hera"); // !!! THIS WAS IMPORTANT we need to show the beginning of a LIST
+		////TO DO implement better find fucntion that goes forward and backward to find an element
+
+		print_all(norse_gods);
+		cout << endl;
+		print_all(greek_gods);
+		cout << endl;
+	}
+
+
 	void homework_17()
 	{
 		//hw_1_2();
 		//hw3();
 		//hw4();
-		hw5();
-	}
+		//hw5();
+		//hw6();
+		hw11(); //TO DO implement better find fucntion that goes forward and backward to find an element
+	}	
+
+	
 
 }
--- Prace domowe/Rozdział_17/Homework_17.h
+++ Prace domowe/Rozdział_17/Homework_17.h
@@ -4,6 +4,28 @@
 
 namespace Hw17
 {
+	class Link
+	{
+	public:
+		string value;
+		Link(const string& v, Link* p = 0, Link * s = 0) :
+			value(v), prev(p), succ(s) {}
+		Link* insert(Link* n);
+		Link* add(Link* n);
+		Link* erase();
+		Link* find(const string& s);
+		Link* Link::find_rev(const string& s);
+		Link* advance(int n) ;
+		Link* Link::move(int n);
+
+		Link* next() const { return succ; }
+		Link* previous() const{ return prev; }
+		
+	private:
+		Link* prev;
+		Link* succ;
+	};
+
 
 	void homework_17();
 	void hw_1_2();
@@ -15,5 +37,7 @@ namespace Hw17
 	void hw4();
 	const char* findx(const char* s, const char* x);
 	void hw5();
+	void hw6();
+	Link* insert(Link* p, Link* n);
 
 }
